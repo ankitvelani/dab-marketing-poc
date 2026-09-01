4b71de567bdf5a86acb053be19c00f315badbc57
ankit.velani@gmail.com <ankit.velani@gmail.com>
2026-09-01 06:36:58 +0000

    initial setup

---
+++ marketing/src/framework/autoloader.py
@@ -0,0 +1,34 @@
+# Databricks notebook source
+from pyspark.sql import functions as F
+
+# COMMAND ----------
+
+def source_to_bronze(source_system,
+                     file_format,
+                     schema_location,
+                     landing_path,
+                     checkpoint_location,
+                     bronze_table):
+    """
+    """
+    df = spark.readStream\
+        .format("cloudFiles")\
+        .option("cloudFiles.format", file_format)\
+        .option("cloudFiles.schemaLocation", schema_location)\
+        .option("cloudFiles.schemaEvolutionMode", "rescue")\
+        .option("cloudFiles.inferColumnTypes", "true")\
+        .option("cloudFiles.maxFilesPerTrigger", "1000")\
+        .option("cloudFiles.validateOptions", "true")\
+        .load(landing_path)\
+        .withColumn("_source_system", F.lit(source_system))\
+        .withColumn("_ingestion_ts", F.current_timestamp())\
+        .withColumn("_source_file", F.col("_metadata.file_path"))
+
+
+    df.writeStream\
+        .format("delta")\
+        .option("checkpointLocation", checkpoint_location)\
+        .option("mergeSchema", "true")\
+        .trigger(availableNow=True)\
+        .outputMode("append")\
+        .toTable(bronze_table)
