8e7d39da6e4630661df45cf2ee32ecc202fa6e67
ankit.velani@gmail.com <ankit.velani@gmail.com>
2026-09-02 08:54:22 +0000

    fixed the workflow for Google and FB

--- marketing/src/facebook_ads.py
+++ marketing/src/facebook_ads.py
@@ -9,10 +9,9 @@
 
 # DBTITLE 1,Setup catalog and schema
 # Set default catalog and schema
-dbutils.widgets.text("catalog", "dev", "Catalog")
-dbutils.widgets.text("schema", "marketing", "Schema")
 catalog = dbutils.widgets.get("catalog")
 schema = dbutils.widgets.get("schema")
+entity = dbutils.widgets.get("entity")
 spark.sql(f"USE CATALOG `{catalog}`")
 spark.sql(f"CREATE SCHEMA IF NOT EXISTS `{schema}`")
 spark.sql(f"USE SCHEMA `{schema}`")
@@ -23,25 +22,14 @@ spark.sql(f"CREATE VOLUME IF NOT EXISTS `{catalog}`.`{schema}`.`{volume}`")
 
 # COMMAND ----------
 
-FACEBOOK_ADS_ENTITIES = ["facebook_ads__account_report",
-                        "facebook_ads__campaign_report",
-                        "facebook_ads__ad_set_report",
-                        "facebook_ads__ad_report",
-                        "facebook_ads__country_report"]
+dataset_name = "Facebook"
+landing_root = "/Volumes/source/raw/datasets/"
 
-def build_entity_configs(dataset_name, entities: list[str], landing_root: str, catalog_schema: str):
-    return [
-       {
-        "source_system": f"{dataset_name}_{e}",
-        "file_format": "csv",
-        "schema_location": f"/Volumes/{catalog}/{schema}/{volume}/schema/{dataset_name}/{e}",
-        "landing_path": f"{landing_root}/{dataset_name}/{e}/*",
-        "checkpoint_location": f"/Volumes/{catalog}/{schema}/{volume}/checkpoint/{dataset_name}/{e}",
-        "bronze_table": f"{catalog_schema}.{e}"
-        }
-        for e in entities
-    ]
-
-configs  = build_entity_configs("Facebook", FACEBOOK_ADS_ENTITIES, "/Volumes/source/raw/datasets/", f"{catalog}.{schema}")
-for cfg in configs:
-    source_to_bronze(**cfg)
+source_to_bronze(
+    source_system=entity,
+    file_format="csv",
+    schema_location=f"/Volumes/{catalog}/{schema}/{volume}/schema/{dataset_name}/{entity}",
+    landing_path=f"{landing_root}{dataset_name}/{entity}",
+    checkpoint_location=f"/Volumes/{catalog}/{schema}/{volume}/checkpoint/{dataset_name}/{entity}",
+    bronze_table=f"{catalog}.{schema}.{entity}",
+)
